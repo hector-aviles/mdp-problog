8525f0072711a7472734b50f82cff3c6d13c26a0
pumpkinnlatte <2230343@upv.edu.mx>
2026-03-13 17:37:56 -0600

    feat: created new fluent structure

---
+++ mdpproblog/fluent/__init__.py
@@ -0,0 +1,8 @@
+from mdpproblog.fluent.fluent import Fluent, StateSpace, ActionSpace
+#from mdpproblog.fluent.spaces 
+#from mdpproblog.fluent.classification 
+from mdpproblog.fluent.exceptions import (
+    MDPProbLogError, FluentDeclarationError,
+    FluentInferenceError, FluentCardinalityError,
+    FluentMassConservationError
+)
